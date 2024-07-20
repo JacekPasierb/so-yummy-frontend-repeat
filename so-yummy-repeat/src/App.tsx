57dbe15c3506d8e10c939ed88e97de40ef4f617c
Jacek Pasierb <jack_byk@o2.pl>
2024-07-20 19:00:24 +0200

    Feature/App - created base route

---
+++ so-yummy-repeat/src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import WelcomePage from "./components/pages/WelcomePage";
+import SigninPage from "./components/pages/SigninPage";
+import RegisterPage from "./components/pages/RegisterPage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/login" element={<SigninPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
